95ae238378eb86617aa9e3db9974fb73de2c522c
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-11-29 09:35:25 +0100

    creación de la cuenta estilo

--- html/crearCuenta.php
+++ html/crearCuenta.php
@@ -57,55 +57,102 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $conn->close();
 }
 ?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
 </head>
 
 <body>
-    <h2>Registro</h2>
+  <header>
+    <!-- place navbar here -->
+  </header>
+  <main>
+  <div class="container login-container">
+
+  <h2 class="text-center">Crear cuenta</h2>
     <?php if (isset($error_message)) : ?>
         <p style="color: red;"><?php echo $error_message; ?></p>
     <?php endif; ?>
     <form method="post" action="">
-        <label for="dni">DNI:</label>
-        <input type="text" id="dni" name="dni" required><br>
+    <div class="mb-3">
+        <label for="dni" class="form-label">DNI:</label>
+        <input type="dni" class="form-control" id="dni" name="dni" required>
+      </div>
 
-        <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" required><br>
+      <div class="mb-3">
+        <label for="nombre_usuario" class="form-label">Nombre Usuario:</label>
+        <input type="nombre_usuario" class="form-control" id="nombre_usuario" name="nombre_usuario" required>
+      </div>
 
-        <label for="apellido">Apellido:</label>
-        <input type="text" id="apellido" name="apellido" required><br>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br>
+      <div class="mb-3">
+        <label for="apellido" class="form-label">Apellido:</label>
+        <input type="apellido" class="form-control" id="apellido" name="apellido" required>
+      </div>
 
-        <label for="fecha_nacimiento">Fecha de Nacimiento:</label>
-        <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" required><br>
 
-        <label for="direccion">Dirección:</label>
-        <input type="text" id="direccion" name="direccion" required><br>
+        <div class="mb-3">
+        <label for="email" class="form-label">email:</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+      </div>
 
-        <label for="codigo_postal">Código Postal:</label>
-        <input type="text" id="codigo_postal" name="codigo_postal" required><br>
+        <div class="mb-3">
+        <label for="fecha_nacimiento" class="form-label">Fecha Nacimiento:</label>
+        <input type="fecha_nacimiento" class="form-control" id="fecha_nacimiento" name="fecha_nacimiento" required>
+      </div>
 
-        <label for="ciudad">Ciudad:</label>
-        <input type="text" id="ciudad" name="ciudad" required><br>
+        <div class="mb-3">
+        <label for="direccion" class="form-label">Direccion:</label>
+        <input type="direccion" class="form-control" id="direccion" name="direccion" required>
+      </div>
+        <div class="mb-3">
+        <label for="codigo_postal" class="form-label">Codigo Postal:</label>
+        <input type="codigo_postal" class="form-control" id="codigo_postal" name="codigo_postal" required>
+      </div>
 
-        <label for="provincia">Provincia:</label>
-        <input type="text" id="provincia" name="provincia" required><br>
+        <div class="mb-3">
+        <label for="ciudad" class="form-label">Ciudad:</label>
+        <input type="ciudad" class="form-control" id="ciudad" name="ciudad" required>
+      </div>
 
-        <label for="contrasena">Contraseña:</label>
-        <input type="password" id="contrasena" name="contrasena" required><br>
+        <div class="mb-3">
+        <label for="provincia" class="form-label">Provincia:</label>
+        <input type="provincia" class="form-control" id="provincia" name="provinicia" required>
+      </div>
 
-        <input type="submit" value="Registrar">
-    </form>
+        <div class="mb-3">
+        <label for="contrasena" class="form-label">Contraseña:</label>
+        <input type="password" class="form-control" id="contrasena" name="contrasena" required>
+      </div>
+
+        <input type="submit" class="btn btn-primary" value="Registrar">
     <p>¿Ya tienes una cuenta? <a href="login.php">Iniciar sesión</a></p>
+    
+</form>
+</div>
+
+</main>
+  <footer>
+    <!-- place footer here -->
+  </footer>
+  
 </body>
 
 </html>
